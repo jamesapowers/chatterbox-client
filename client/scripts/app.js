5aabc46f0d6ba8b005261ac4c2e269e5203b0e53
James Powers <powersajames@gmail.com>
2018-01-27 20:59:16 -0800

    added friendship

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,12 @@
-// YOUR CODE HERE:
 var app = {
   server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages'
 };
-// $(document).ready(function () {
 
 app.init = function (username) {
+  let url = new URL(window.location.href);
 };
+
 app.send = function (message) {
-  //return true;
   $.ajax({
     type: "POST",
     url: "http://parse.sfm8.hackreactor.com/chatterbox/classes/messages",
@@ -19,30 +18,25 @@ app.send = function (message) {
       console.error('chatterbox: Failed to send message', data);
     }
   });
-  // $.post("http://parse.sfm8.hackreactor.com/", JSON.stringify(message), function (data) {return JSON.stringify(data);}, 'json');
 };
 
-// app.isInRoomList(string) {
-//   // iterate thru rooms
-//   // 
-//   return boolean;
-// }
 app.fetch = function () {
+  app.clearMessages();
   var postMessages = function (serverData) {
-    for (var i = 0; i < serverData.results.length; i ++) {
+    console.log(serverData);
+    for (var i = serverData.results.length - 1; i >= 0; i--) {
       app.renderMessage(serverData.results[i]);
     }
-    console.log(serverData);
   };
   $.ajax({
     type: "GET",
     url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
     success: postMessages,
+    data: {'order': '-createdAt'},
     error: function (data) {
       console.error('chatterbox: Failed to get message');
     }
   });
-  return 1;
 };
 
 app.clearMessages = function () {
@@ -59,61 +53,68 @@ app.makeMessageSafe = function (message) {
     "'": '&#x27',
     "/": '&#x2F' 
   };
-  for (i = 0; i < message.text.length; i++) {
-    if (message.text[i] in badChar) {
-      cleanMessage = cleanMessage.concat(badChar[message.text[i]]);
+  if (message === undefined) { return; }
+  for (i = 0; i < message.length; i++) {
+    if (message[i] in badChar) {
+      cleanMessage = cleanMessage.concat(badChar[message[i]]);
     } else {
-      cleanMessage = cleanMessage.concat(message.text[i]);
+      cleanMessage = cleanMessage.concat(message[i]);
     }
   }
   return cleanMessage;
 };
 
 app.renderMessage = function (message) {
-  
+  var userName = message.username;
+  if (userName === undefined || userName === '') {
+    userName = 'anonymous';
+  }
   var roomValue = document.getElementById('roomSelect');
+  var selectedRoom = roomValue[roomValue.selectedIndex].value;
   
-  if (message.roomname === roomValue[roomValue.selectedIndex].value) {
-    var cleanMessage = app.makeMessageSafe(message);  
-    var prependNode = "<div id = 'chatMessage'>" + cleanMessage + "</div>";
-    $('#chats').prepend(prependNode); 
+  if (message.roomname === selectedRoom || selectedRoom === 'Lobby') {
+    var cleanMessage = app.makeMessageSafe(message.text);  
+    var prependMessage = "<div class = 'username chatMessage " + userName + "' " + "data-user="+ userName + " >" +  userName +  " sent:  <br>" + cleanMessage + "</div>";
+    $('#chats').prepend(prependMessage); 
   }
   
 };
 
 app.renderRoom = function (roomName) {
   roomName = roomName || document.getElementById('roomID').value;
-  var newRoom = "<option value=" + roomName + ">" + roomName + "</option>";
+  if (roomName.length === 0) { return; }
+  var newRoom = "<option value=" + roomName + " selected>" + roomName + "</option>";
   $('#roomSelect').append(newRoom);
+  app.fetch();
+  document.getElementById('roomID').value = '';
 };
   
 $(document).ready(function () {
   app.sendMessage = function (message) {
     var x = document.getElementById("message").value;
-    document.getElementById("chats").innerHTML = x;
-    console.log(x);
+    var url = new URL(window.location.href);
+    var roomValue = document.getElementById('roomSelect');
+    var user = app.makeMessageSafe(url.searchParams.get('username').toString());
+    var selectedRoom = roomValue[roomValue.selectedIndex].value;
     var message = {
-      username: "<script> var styleSettings = { 'background-image': \"url('https:\/\/tinyurl.com/ybq2cy5u')\"}; $('body').css(styleSettings);</script>",
+      username: user,
       text: x,
+      roomname: selectedRoom
     };
     app.send(message);
+    app.fetch();
     document.getElementById('message').value = '';
   };
-  
+  document.getElementById("roomID").onchange = function () {
+    app.clearMessages();
+    app.fetch();
+  };
+  $(document.body).on('click', '.username', function () {
+    var styleSettings = {
+      'font-weight': 800,
+    };
+    var user = $(this).data('user');
+    $('.' + user).css(styleSettings);
+  });
 });
-
-
-// <script> var styleSettings = { 'background-image': \"url('https:\/\/tinyurl.com/ybq2cy5u')\"}; $('body').css(styleSettings);</script>
-// 
-
-
-
-
-
-
-
-
-
-
-
 
